81109cc2ff35a49eb4f690072fdca17e97453d06
MelodyDuplaix <melo.surseine@gmail.com>
2025-01-08 10:09:46 +0100

    Feat: Mise en place de l'ui (input, button, function)

--- AppStreamlit/app.py
+++ AppStreamlit/app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+def detect_spam(sms):
+    if sms == "":
+        return False
+    else:
+        return True
+is_spam = False
+
 st.set_page_config(
     page_title="Spam Detector",
     page_icon="🔕",
@@ -8,3 +15,14 @@ st.set_page_config(
 )
 
 st.title("Spam Detector")
+st.header("To detect if the sms is a spam")
+sms = st.text_area("Enter the sms here", key="sms", height=150)
+with st.columns(5)[2]:
+    if st.button("Detect", use_container_width=True):
+        is_spam = detect_spam(sms)
+        
+if is_spam:
+    st.error("The sms is a spam")
+else:
+    st.success("The sms is not a spam")
+
