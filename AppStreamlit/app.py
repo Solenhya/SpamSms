0217be3e325d61e9377a0f00786e44b6abdf02cf
MelodyDuplaix <melo.surseine@gmail.com>
2025-01-08 10:26:56 +0100

    Fix: Ajout du comment todo

--- AppStreamlit/app.py
+++ AppStreamlit/app.py
@@ -4,8 +4,10 @@ def detect_spam(sms):
     if sms == "":
         return False
     else:
+        # Todo: Implement spam detection logic
         return True
 is_spam = False
+
 
 st.set_page_config(
     page_title="Spam Detector",
