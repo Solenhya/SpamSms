e772e51b3c7d39026c43f2959e121be639790861
MelodyDuplaix <melo.surseine@gmail.com>
2025-01-08 09:51:39 +0100

    feat: Mise en place de l'app

---
+++ AppStreamlit/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analyse associations",
+    page_icon="📊",
+    menu_items={
+        "Get Help": "https://www.cefim.eu/",
+        "About" : "https://www.linkedin.com/in/melody-duplaix-391672265"
+    }
+)
+
+st.title("Spam Detector")
