a97a2947d8ba200683bc5a71d4c6ab6370472ee7
MelodyDuplaix <melo.surseine@gmail.com>
2025-01-13 16:53:50 +0100

    Refactor: Factorisation pour enlever le split test_train du module

--- SKLearnModels.py
+++ SKLearnModels.py
@@ -13,22 +13,20 @@ import re
 
 class BaseCountModel():
     
-    def __init__(self, dataset, random=None):
-        """Initie la base du modèle
+    def __init__(self, X, y, random=None):
+        """Applique un modèle de classification sur des données
 
         Args:
-            dataset (DataFrame): DataFrame d'entrainement du modèle
-            random (Booléean, optional): Si True, split random établit à 42. Defaults to None.
+            X (Série): Série d'entrainement de features
+            y (Série): Série d'entrainement de label
+            random (Booléan, optional): Si True randomise avec une seed de 42. Defaults to None.
         """
-        self.dataset = dataset
-        self.dataset['spam'] = self.dataset['spam'].replace({'ham': 0, 'spam': 1})
+        self.y = y
         self.vectorizer = CountVectorizer(stop_words='english')
-        self.X = self.vectorizer.fit_transform(self.dataset['text'])
-        self.y = self.dataset['spam']
+        self.X = self.vectorizer.fit_transform(X)
         self.random_state = 42 if random else None
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
         
-    def Predict(self, test_data=None):
+    def Predict(self, test_data):
         """Applique le modèle sur des données
 
         Args:
@@ -37,31 +35,26 @@ class BaseCountModel():
         Returns:
             list: Prédictions sous forme de labels ('ham' ou 'spam').
         """
-        if test_data is None:
-            test_data = self.X_test
-        else:
-            test_data = self.vectorizer.transform(test_data)
+        test_data = self.vectorizer.transform(test_data)
         predictions = self.model.predict(test_data)
         return predictions
     
 class BaseTFIDModel():
     
-    def __init__(self, dataset, random=None):
-        """Initie la base du modèle
+    def __init__(self, X, y, random=None):
+        """Applique un modèle de classification sur des données
 
         Args:
-            dataset (DataFrame): DataFrame d'entrainement du modèle
-            random (Booléean, optional): Si True, split random établit à 42. Defaults to None.
+            X (Série): Série d'entrainement de features
+            y (Série): Série d'entrainement de label
+            random (Booléan, optional): Si True randomise avec une seed de 42. Defaults to None.
         """
-        self.dataset = dataset
-        self.dataset['spam'] = self.dataset['spam'].replace({'ham': 0, 'spam': 1})
-        self.vectorizer = TfidfVectorizer(stop_words='english')
-        self.X = self.vectorizer.fit_transform(self.dataset['text'])
-        self.y = self.dataset['spam']
+        self.y = y
+        self.vectorizer = CountVectorizer(stop_words='english')
+        self.X = self.vectorizer.fit_transform(X)
         self.random_state = 42 if random else None
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
         
-    def Predict(self, test_data=None):
+    def Predict(self, test_data):
         """Applique le modèle sur des données
 
         Args:
@@ -70,29 +63,24 @@ class BaseTFIDModel():
         Returns:
             list: Prédictions sous forme de labels ('ham' ou 'spam').
         """
-        if test_data is None:
-            test_data = self.X_test
-        else:
-            test_data = self.vectorizer.transform(test_data)
+        test_data = self.vectorizer.transform(test_data)
         predictions = self.model.predict(test_data)
         return predictions
 
 class BaseFeaturesModel():
 
-    def __init__(self, dataset, random=None):
-        """Initie la base du modèle
+    def __init__(self, X, y, random=None):
+        """Applique un modèle de classification sur des données
 
         Args:
-            dataset (DataFrame): DataFrame d'entrainement du modèle
-            random (Booléean, optional): Si True, split random établit à 42. Defaults to None.
+            X (Série): Série d'entrainement de features
+            y (Série): Série d'entrainement de label
+            random (Booléan, optional): Si True randomise avec une seed de 42. Defaults to None.
         """
-        self.dataset = dataset
-        self.dataset['spam'] = self.dataset['spam'].replace({'ham': 0, 'spam': 1})
-        self.dataset = self.Vectorize(self.dataset)
-        self.X = self.dataset.drop(columns=['text', 'spam'])
-        self.y = self.dataset['spam']
+        self.y = y
+        self.X = self.Vectorize(X)
+        self.X = self.X.drop(columns=['text'])
         self.random_state = 42 if random else None
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
         
     def Vectorize(self, df):
         """Transforme les données en vecteurs
@@ -102,17 +90,19 @@ class BaseFeaturesModel():
         Returns:
             list: Vecteurs de données
         """
+        df = pd.DataFrame(df, columns=['text'])
         df['has_phone_number'] = df['text'].apply(lambda x: 1 if re.search(r'\b\d{10,}\b', x) else 0)
         df['has_currency_symbol'] = df['text'].apply(lambda x: 1 if re.search(r'[\$\€\£]', x) else 0)
         df["has_special_characters"] = df['text'].apply(lambda x: 1 if re.search(r'[!@#$%^&*(),.?":{}|<>]', x) else 0)
         df['message_length'] = df['text'].apply(len)
+        df["number_count"] = df['text'].apply(lambda x: sum(c.isdigit() for c in x))
         df['word_count'] = df['text'].apply(lambda x: len(x.split()))
         df["uppercase_proportion"] = df['text'].apply(lambda x: sum(1 for c in x if c.isupper()) / len(x) if len(x) > 0 else 0)
         df["has_url"] = df['text'].apply(lambda x: 1 if re.search(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', x) else 0)
         df["non_alpha_charaters_proportion"] = df['text'].apply(lambda x: sum(1 for c in x if not c.isalpha()) / len(x) if len(x) > 0 else 0)
         return df
         
-    def Predict(self, test_data=None):
+    def Predict(self, test_data):
         """Applique le modèle sur des données
 
         Args:
@@ -121,11 +111,8 @@ class BaseFeaturesModel():
         Returns:
             list: Prédictions sous forme de labels ('ham' ou 'spam').
         """
-        if test_data is None:
-            test_data = self.X_test
-        else:
-            test_data = self.Vectorize(pd.DataFrame(test_data, columns=['text']))
-            test_data = test_data.drop(columns=['text'])
+        test_data = self.Vectorize(pd.DataFrame(test_data, columns=['text']))
+        test_data = test_data.drop(columns=['text'])
         predictions = self.model.predict(test_data)
         return predictions
 
@@ -133,88 +120,88 @@ class BaseFeaturesModel():
 
 class NaiveBayesModel(BaseCountModel):
     
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
 
         Args:
             dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = MultinomialNB()
-        self.model.fit(self.X_train, self.y_train)
+        self.model.fit(self.X, self.y)
         
         
 class NaiveBayesFeaturesModel(BaseFeaturesModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         
         Args:
             dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = MultinomialNB()
-        self.model.fit(self.X_train, self.y_train)
+        self.model.fit(self.X, self.y)
         
 class SCVModel(BaseCountModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         
         Args:
             dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = LinearSVC()
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
 
 class SCVFeaturesModel(BaseFeaturesModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
 
         Args:
             dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = LinearSVC()
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
 
 class RandomForestModel(BaseTFIDModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         Args:
                 dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = RandomForestClassifier(max_depth=20)
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
 
 class RandomForestFeaturesModel(BaseFeaturesModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         Args:
                 dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = RandomForestClassifier()
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
 
 
 class LogisticRegressionModel(BaseCountModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         Args:
                 dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = LogisticRegression()
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
         
 class LogisticRegressionFeaturesModel(BaseFeaturesModel):
-    def __init__(self, dataset, random=None):
+    def __init__(self, X, y, random=None):
         """Créer un modèle de classification
         Args:
                 dataset (Dataframe): Dataframe d'entrainement du modèle
         """
-        super().__init__(dataset, random)
+        super().__init__(X, y, random)
         self.model = LogisticRegression()
-        self.model.fit(X=self.X_train, y=self.y_train)
+        self.model.fit(X=self.X, y=self.y)
 
--- TestSKlearnModelReader.py
+++ TestSKlearnModelReader.py
@@ -1,35 +1,38 @@
+from re import X
 from SKLearnModels import NaiveBayesModel, SCVModel, RandomForestModel, LogisticRegressionModel, NaiveBayesFeaturesModel
 import pandas as pd
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("BD1.txt", sep="\t", names=["spam", "text"])
+X_train, X_test, y_train, y_test = train_test_split(df["text"], df["spam"], test_size=0.2, random_state=42)
 
 print("---------------------------------------------")
-model = NaiveBayesModel(df, random=42)
+model = NaiveBayesModel(X_train,y_train, random=42)
 print("NaiveBayesModel")
-print(classification_report(model.y_test, model.Predict()))
+print(classification_report(y_test, model.Predict(X_test)))
 print(model.Predict(test_data=["You won a free holiday to the Maldives! Call 09066364304 now!!"]))
 
 print("---------------------------------------------")
-model = NaiveBayesFeaturesModel(df, random=42)
+model = NaiveBayesFeaturesModel(X_train,y_train, random=42)
 print("NaiveBayesFeaturesModel")
-print(classification_report(model.y_test, model.Predict()))
+print(classification_report(y_test, model.Predict(X_test)))
 print(model.Predict(test_data=["You won a free holiday to the Maldives! Call 09066364304 now!!"]))
 
 print("----------------------------------------------")
 print("LinearSVCModel")
-model = SCVModel(df, random=42)
-print(classification_report(model.y_test, model.Predict()))
+model = SCVModel(X_train,y_train, random=42)
+print(classification_report(y_test, model.Predict(X_test)))
 print(model.Predict(test_data=["You won a free holiday to the Maldives! Call 09066364304 now!!"]))
 
 print("----------------------------------------------")
 print("RandomForestModel")
-model = RandomForestModel(df, random=42)
-print(classification_report(model.y_test, model.Predict()))
+model = RandomForestModel(X_train,y_train, random=42)
+print(classification_report(y_test, model.Predict(X_test)))
 print(model.Predict(test_data=["You won a free holiday to the Maldives! Call 09066364304 now!!"]))
 
 print("----------------------------------------------")
 print("LogisticRegressionModel")
-model = LogisticRegressionModel(df, random=42)
-print(classification_report(model.y_test, model.Predict()))
+model = LogisticRegressionModel(X_train,y_train, random=42)
+print(classification_report(y_test, model.Predict(X_test)))
 print(model.Predict(test_data=["You won a free holiday to the Maldives! Call 09066364304 now!!"]))
